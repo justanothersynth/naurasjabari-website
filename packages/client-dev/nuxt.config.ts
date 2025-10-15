cddc4f27c8e8d775eac2223948dfcdb9e706a614
Nauras Jabari <naurasj@gmail.com>
2025-10-15 09:49:29 -0400

    feat: favicon now changes based on browser theme

--- packages/client-dev/nuxt.config.ts
+++ packages/client-dev/nuxt.config.ts
@@ -65,11 +65,11 @@ export default defineNuxtConfig({
     head: {
       title: 'Nauras Jabari',
       meta: [
-        { name: 'msapplication-config', content: '/favicon/light/browserconfig.xml?v=0' }
+        { name: 'msapplication-config', content: '/favicon/dark/browserconfig.xml?v=0' }
       ],
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon/light/favicon-96x96.png?v=0' },
-        { rel: 'manifest', href: '/favicon/light/manifest.json?v=0' }
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon/dark/favicon-96x96.png?v=0' },
+        { rel: 'manifest', href: '/favicon/dark/manifest.json?v=0' }
       ]
     }
   },
