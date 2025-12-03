a3d26bbe9423400b08c6ea964266327531398c1c
Nauras Jabari <naurasj@gmail.com>
2025-12-03 13:05:27 -0500

    feat: removed "under construction" disclaimer

--- packages/client-dev/app/pages/index.vue
+++ packages/client-dev/app/pages/index.vue
@@ -5,18 +5,6 @@
     
     <div class="max-w-4xl mx-auto">
 
-      <div class="flex bg-red-200 py-2 px-3 mb-20 rounded-lg text-left">
-        <span>🚧</span>
-        <p class="mx-2">
-          This website is currently under construction and is being actively worked upon.
-          Service interruptions or errors may occur.
-          Fireworks may fly.
-          Eyes may melt.
-          Extreme feelings of being impressed may overwhelm you.
-        </p>
-        <span>🚧</span>
-      </div>
-
       <Intro />
 
       <GithubContributionsGraph />
