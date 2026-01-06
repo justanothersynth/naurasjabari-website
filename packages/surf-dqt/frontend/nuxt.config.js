51036757ceb2099f9604cfb5573f3dd9447d2bef
Nauras Jabari <naurasj@gmail.com>
2026-01-06 11:06:30 -0500

    feat: specified publicPath in attempt to resolve build issues

--- packages/surf-dqt/frontend/nuxt.config.js
+++ packages/surf-dqt/frontend/nuxt.config.js
@@ -225,6 +225,8 @@ export default {
   // /////////////////////////////////////////////////////// Build configuration
   // ------------------------------------------------ Extend webpack config here
   build: {
+    // ------------------------------------------------------------- Public Path
+    publicPath: '/surf-dqt/_nuxt/',
     // ---------------------------------------------------------- Hot Middleware
     hotMiddleware: {
       client: {
